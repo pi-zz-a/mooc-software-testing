0adc718549e9c2f2c614a7a518a13cb72f2194e6
pia <piakeuk@hotmail.com>
2018-11-03 19:05:00 +0100

    remove bug from CaesarShiftCipher and add tests for that class

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,32 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    public void testMiddleOfAlphabetPosShift() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("df", csc.CaesarShiftCipher("ce",1));
+    }
+
+    @Test
+    public void testMiddleOfAlphabetNegShift() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("bd", csc.CaesarShiftCipher("ce",-1));
+    }
+
+    @Test
+    public void testBeginOfAlphabetNegShift() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("yz",csc.CaesarShiftCipher("ab", -2));
+    }
+
+    @Test
+    public void testEndOfAlphabetPosShift() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("zb",csc.CaesarShiftCipher("xz", 2));
+    }
 }
