79467c3da8c37aa9dbbaef59f9df71f8cba0d74c
pia <piakeuk@hotmail.com>
2018-11-03 18:53:18 +0100

    add tests for gHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,34 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GHappyTest {
+
+    @Test
+    public void testSingleG() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("g"));
+    }
+
+    @Test
+    public void testSingleSurroundedG() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("xgx"));
+    }
+
+    @Test
+    public void testDoubleG() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("gg"));
+    }
+
+    @Test
+    public void testDoubleGNotNeighbours() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("xgxg"));
+    }
+
 }
