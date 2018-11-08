68eebb242f0e9dadfc8310eb69f48dcdbbc436d6
pia <piakeuk@hotmail.com>
2018-11-08 12:53:49 +0100

    solve bug in mirror and add tests

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,28 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MirrorTest {
+    private Mirror mirror;
+
+    @BeforeEach
+    public void setUp() {
+        mirror = new Mirror();
+    }
+
+    @Test
+    public void testEvenInput() {
+
+        String mirrorEnds = mirror.mirrorEnds("abba");
+        assertEquals("abba", mirrorEnds);
+    }
+
+    @Test
+    public void testUnevenInput() {
+        String mirrorEnds = mirror.mirrorEnds("aba");
+        assertEquals("aba", mirrorEnds);
+    }
 }
